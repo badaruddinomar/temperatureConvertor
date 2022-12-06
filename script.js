ee8a50503eb5f3f92f855694a23653b90b8f286f
Badar <badaruddinomar2403@gmail.com>
2022-12-06 21:16:45 +0800

    responsive code added

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-`use strict`;
+// SELECTOR----
+const result = document.querySelector(`.result`);
+const inputFah = document.querySelector(`.fah-val`);
+const inputCel = document.querySelector(`.cel-val`);
 
-// * selector
-const btnCTF = document.querySelector(`.btnCTF`);
-const btnFTC = document.querySelector(`.btnFTC`);
-const btnReset = document.querySelector(`.btnReset`);
+const celBtn = document.querySelector(`.cel-btn`);
+const fahBtn = document.querySelector(`.fah-btn`);
+const resetBtn = document.querySelector(`.reset`);
 
-const celsius1 = document.querySelector(`.cel1`);
-const fahrenheit1 = document.querySelector(`.fah1`);
-
-const celsius2 = document.querySelector(`.cel2`);
-const fahrenheit2 = document.querySelector(`.fah2`);
-
-// * convert celsius to fahrenheit - eventListener
-btnCTF.addEventListener(`click`, function () {
-  let celsiusValue = Number(celsius1.value);
-  let f = (9 * celsiusValue + 160) / 5;
-  fahrenheit1.value = f;
+// --------------------------------------------------
+inputFah.value = ``;
+inputCel.value = ``;
+// FUNCTIONS-------
+// CELCIUS TO FAHRENHEIT CONVERTION--------
+celBtn.addEventListener(`click`, function () {
+  const celVal = +inputCel.value;
+  const fahFormula = (9 * celVal + 160) / 5;
+  result.textContent = `${fahFormula.toFixed(2)}℉`;
+  inputCel.value = ``;
 });
-// * convert fahrenheit to celsius - eventListener
-btnFTC.addEventListener(`click`, function () {
-  let fahrenheitValue = Number(fahrenheit2.value);
-  let c = ((fahrenheitValue - 32) * 5) / 9;
-  celsius2.value = c;
+// FAHRENHEIT TO CELCIUS CONVERTION-----------
+fahBtn.addEventListener(`click`, function () {
+  const fahVal = +inputFah.value;
+  const celFormula = ((fahVal - 32) * 5) / 9;
+  result.textContent = `${celFormula.toFixed(2)}℃`;
+  inputFah.value = ``;
 });
-// * btnResetListener
-btnReset.addEventListener(`click`, function () {
-  celsius1.value = "";
-  fahrenheit1.value = "";
-  celsius2.value = "";
-  fahrenheit2.value = "";
+// RESETING THE INPUT VALUE AND THE RESULT ----------
+resetBtn.addEventListener(`click`, function () {
+  inputFah.value = ``;
+  inputCel.value = ``;
+  result.textContent = `Result: `;
 });
